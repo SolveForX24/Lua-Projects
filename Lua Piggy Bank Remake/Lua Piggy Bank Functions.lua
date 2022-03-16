b0a63873622e5dd071ce7b4862f7f11c319b2870
SolveForX_ <86010571+SolveForX24@users.noreply.github.com>
2022-03-16 10:23:29 -0400

    The functions are now in lua, not c++

--- Lua Piggy Bank Remake/Lua Piggy Bank Functions.lua
+++ Lua Piggy Bank Remake/Lua Piggy Bank Functions.lua
@@ -1,7 +1,7 @@
 -- Piggy Bank Function
 -- Calculates the total based on input and conversion factors.
 function calculateTotalReturn(p, n, d, q) 
-	local toReturn = ((p)+(n * 5) + (d * 10) + (q * 25)) / 100.0;
+	local toReturn = ((p)+(n * 5) + (d * 10) + (q * 25)) / 100.0
 	return toReturn;
 end
 
@@ -13,29 +13,30 @@ end
     
 -- Returns 52 times the saving rate to show the amount saved in a year.
 function calculateSavingsPerYearReturn(timeSaved, p, n, d, q)
-    local toReturn = 52 * calculateSavingRateReturn(timeSaved, p, n, d, q);
+    local toReturn = 52 * calculateSavingRateReturn(timeSaved, p, n, d, q)
     return toReturn;
 end
 
 function printSavingsStatement(timeSaved, p, n, d, q, name) 
 
-        -- First chunk
-        print(name .. ", you have saved $" .. calculateTotalReturn(p, n, d, q) .. " overall.\nThat's $" .. calculateSavingRateReturn(timeSaved, p, n, d, q) .. " a week!";
-        // If saving for more then a year, calculate the specific amount saved in the year.
-        if (timeSaved > 52) {
-            cout  "\nGreat job, " .. name .. ", you saved $" .. (calculateSavingRateReturn(timeSaved, p, n, d, q) * 52) .. " in one year! You're still on track now to save the same amount in total in the new year!";
-        }
-        // If saving for EXACTLY a year, pr a special message.
-        elseif (timeSaved == 52) 
-            then
-            cout  "\nGreat job, " .. name .. ", you saved $" .. calculateTotalReturn(p, n, d, q) .. " this past year! Can you save that much again in this next year?";
-        } -- Else, just pr how much you could save this year.
-        else 
-        then
-            print("\nGreat job, " .. name .. ", at this rate, you can save $" .. calculateSavingsPerYearReturn(timeSaved, p, n, d, q) .. " this year!")
-        end
+    -- First chunk
+    print(name .. ", you have saved $" .. calculateTotalReturn(p, n, d, q) .. " overall.\nThat's $" .. calculateSavingRateReturn(timeSaved, p, n, d, q) .. " a week!")
+    -- If saving for more then a year, calculate the specific amount saved in the year.
+    if (timeSaved > 52) 
+    then
+        print("\nGreat job, " .. name .. ", you saved $" .. (calculateSavingRateReturn(timeSaved, p, n, d, q) * 52) .. " in one year! You're still on track now to save the same amount in total in the new year!")
     
+        -- If saving for EXACTLY a year, pr a special message.
+    elseif (timeSaved == 52) 
+    then
+        print("\nGreat job, " .. name .. ", you saved $" .. calculateTotalReturn(p, n, d, q) .. " this past year! Can you save that much again in this next year?")
+    
+        -- Else, just pr how much you could save this year.
+    else 
+        print("\nGreat job, " .. name .. ", at this rate, you can save $" .. calculateSavingsPerYearReturn(timeSaved, p, n, d, q) .. " this year!")
     end
+    
+end
 
 -- This function yoinked from here: https://www.codegrepper.com/code-examples/lua/wait+function+lua
 function wait(seconds)
@@ -43,4 +44,4 @@ function wait(seconds)
     repeat until os.time() > start + seconds
   end
 
-return { calculateTotalReturn = calculateTotalReturn, wait = wait }
+return { calculateTotalReturn = calculateTotalReturn, calculateSavingRateReturn = calculateSavingRateReturn, calculateSavingsPerYearReturn = calculateSavingsPerYearReturn, printSavingsStatement = printSavingsStatement, wait = wait }
